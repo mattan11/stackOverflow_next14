4e7b3237e292e21e73b49f5b7505ecbe3ffc8296
Marek Witkowski <marek.witkowski@intersynergy.pl>
2023-12-14 08:34:05 +0100

    routing: add pages for auth, ask-question and homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="h1-bold">
-                Next14
-            </h1>
+            <h1 className="h1-bold">Next14</h1>
         </div>
     )
 }
