d2ed0e0dd09e03a95f4b2f5dd1c25e58e0deacee
Marek Witkowski <marek.witkowski@intersynergy.pl>
2023-12-20 15:09:42 +0100

    change tab indent to 2

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const AskQuestion = () => {
-    return <div>Ask Question</div>
+  return <div>Ask Question</div>
 }
 
 export default AskQuestion
--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from '@clerk/nextjs'
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ['/'],
+  publicRoutes: ['/'],
 })
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
