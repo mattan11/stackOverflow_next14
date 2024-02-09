fd291fedab7b1e60d3835b8dd60f7a74ab4bbe30
Marek Witkowski <marek.witkowski@onet.com.pl>
2024-02-09 08:47:41 +0100

    feature: add form do add new question

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Question from '@/components/forms/Question'
 
 const AskQuestion = () => {
-  return <div>Ask Question</div>
+  return (
+    <div>
+      <div>
+        <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+        <div className="mt-9">
+          <Question />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AskQuestion
