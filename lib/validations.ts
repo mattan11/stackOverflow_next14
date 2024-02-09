130d77187c32b07e982b20d59715e3425a8d999b
Marek Witkowski <marek.witkowski@onet.com.pl>
2024-02-09 13:08:14 +0100

    feature: add editor to ask question and handle tag input

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod'
 
+export const TAG_MAX_LENGTH = 15
+
 export const QuestionsSchema = z.object({
   title: z.string().min(5).max(130),
   explanation: z.string().min(30),
-  tags: z.array(z.string().min(1).max(15)).min(1).max(5),
+  tags: z.array(z.string().min(1).max(TAG_MAX_LENGTH)).min(1).max(3),
 })
