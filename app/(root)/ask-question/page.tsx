e22a89471003b8fa5ab40d32991a0a4f0c257821
Marek Witkowski <marek.witkowski@intersynergy.pl>
2024-03-15 14:43:23 +0100

    feature: get user from MongoDB on question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 import Question from '@/components/forms/Question'
 import { redirect } from 'next/navigation'
 import { getUserById } from '@/lib/actions/user.action'
+import { auth } from '@clerk/nextjs'
 
 const AskQuestion = async () => {
-  // const { userId } = auth()
-
-  const userId = '123456789'
+  const { userId } = auth()
 
   if (!userId) redirect('/sign-in')
 
   const mongoUser = await getUserById({ userId })
 
+  if (!mongoUser) {
+    return <div>User not Found</div>
+  }
+
   return (
     <div>
-      <div>
-        <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-
-        <div className="mt-9">
-          <Question mongoUserId={JSON.stringify(mongoUser._id)} />
-        </div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   )
