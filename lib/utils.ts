560a6c84c44cc31f1be54674c83b0e8bfee5c14c
Marek Witkowski <marek.witkowski@intersynergy.pl>
2024-06-13 15:28:08 +0200

    fix eslint issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -64,11 +64,11 @@ export const getJoinedDate = (date: Date): string => {
   return joinedDate
 }
 
-interface UrlQueryParams {
-  params: string
-  key: string
-  value: string | null
-}
+// interface UrlQueryParams {
+//   params: string
+//   key: string
+//   value: string | null
+// }
 
 // export const formUrlQuery = ({ params, key, value}: UrlQueryParams) => {
 //   const currentUrl = qs.parse(params);
