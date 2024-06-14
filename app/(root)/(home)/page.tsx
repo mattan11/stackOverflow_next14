daa53b9844fcf4a47e425e16a75abce77fc192f1
Marek Witkowski <marek.witkowski@intersynergy.pl>
2024-06-14 11:17:39 +0200

    implement search functionality to home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,9 +7,12 @@ import HomeFilters from '@/components/home/HomeFilters'
 import QuestionCard from '@/components/cards/QuestionCard'
 import { getQuestions } from '@/lib/actions/question.action'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
+import { SearchParamsProps } from '@/types'
 
-export default async function Home() {
-  const result = await getQuestions({})
+export default async function Home({ searchParams }: SearchParamsProps) {
+  const result = await getQuestions({
+    searchQuery: searchParams.q,
+  })
 
   return (
     <>
