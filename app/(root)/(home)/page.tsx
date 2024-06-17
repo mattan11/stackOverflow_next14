bb3ae0c8ea4a321f1742335daf59c078c4425a7f
Marek Witkowski <marek.witkowski@intersynergy.pl>
2024-06-17 08:21:29 +0200

    handle pagination for homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,11 +8,13 @@ import QuestionCard from '@/components/cards/QuestionCard'
 import { getQuestions } from '@/lib/actions/question.action'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
 import { SearchParamsProps } from '@/types'
+import Pagination from '@/components/shared/Pagination'
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const result = await getQuestions({
     searchQuery: searchParams.q,
     filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
   })
 
   // fetch recommended tags
@@ -45,8 +47,8 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {result.length > 0 ? (
-          result.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
@@ -67,6 +69,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
             linkTitle="Ask a Question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   )
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,9 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     await connectToDatabase()
 
-    const { searchQuery, filter } = params
+    const { searchQuery, filter, page = 1, pageSize = 2 } = params
+
+    const skipAmount = (page - 1) * pageSize
 
     const query: FilterQuery<typeof Question> = {}
 
@@ -51,9 +53,14 @@ export async function getQuestions(params: GetQuestionsParams) {
     const questions = await Question.find(query)
       .populate({ path: 'tags', model: Tag })
       .populate({ path: 'author', model: User })
+      .skip(skipAmount)
+      .limit(pageSize)
       .sort(sortOptions)
 
-    return questions
+    const totalQuestions = await Question.countDocuments(query)
+    const isNext = totalQuestions > skipAmount + questions.length
+
+    return { questions, isNext }
   } catch (error) {
     console.error(error)
     throw error
